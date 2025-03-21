4ea045d042a73fd4ae9516af1661fd795016ba51
mochfathurrozi <decozzfx@gmail.com>
2025-03-21 14:45:41 +0700

    feat(appointment): enhance doctor time selection UI; improve layout and add toggle text for appointment visibility

--- apps/isomorphic/src/app/book-appointment/doctor-time.tsx
+++ apps/isomorphic/src/app/book-appointment/doctor-time.tsx
@@ -4,6 +4,7 @@ import ModalBookConfirmation from './book-confirmation';
 import { useAtom } from 'jotai';
 import bookAppointmentAtom from '@/store/book-appointment';
 import Image from 'next/image';
+import { Text } from 'rizzui';
 
 const doctors = [
   {
@@ -104,19 +105,12 @@ const DoctorTime = ({
       </div>
 
       {/* Doctor List */}
-      <div className="mx-auto mt-8 max-w-4xl">
+      <div className="mx-auto mt-8 max-w-4xl divide-y divide-gray-200">
         {doctors.map((doctor, index: number) => (
-          <div key={index} className="mb-5 bg-gray-50">
+          <div key={index} className="mb-5">
             {/* Doctor Header */}
-            <div
-              className="flex cursor-pointer items-center justify-between space-x-4 bg-gray-50 p-6"
-              onClick={() =>
-                setCurrentOpen((prev) =>
-                  prev === doctor.id ? null : doctor.id
-                )
-              }
-            >
-              <div className="flex items-center space-x-4">
+            <div className="flex items-center justify-between space-x-4 p-6">
+              <div className="flex cursor-pointer items-center space-x-4">
                 {doctor.image ? (
                   <Image
                     src={doctor.image}
@@ -137,30 +131,44 @@ const DoctorTime = ({
                   </p>
                 </div>
               </div>
-              <div>
-                {currentOpen === doctor.id ? (
-                  <BiChevronUp size={30} />
-                ) : (
-                  <BiChevronDown size={30} />
-                )}
+              <div
+                className="cursor-pointer"
+                onClick={() =>
+                  setCurrentOpen((prev) =>
+                    prev === doctor.id ? null : doctor.id
+                  )
+                }
+              >
+                <div className="flex items-center gap-2">
+                  {currentOpen === doctor.id ? (
+                    <>
+                      <Text>Hide Appointment</Text> <BiChevronUp size={30} />
+                    </>
+                  ) : (
+                    <>
+                      <Text>See All Appointment</Text>
+                      <BiChevronDown size={30} />
+                    </>
+                  )}
+                </div>
               </div>
             </div>
 
             {/* Doctor Times */}
 
-            <div className="border-b px-4 pb-4">
+            <div className="px-4 pb-4">
               {doctor.times && doctor.times.length > 0 ? (
                 <div
-                  className={`mt-4 grid transition-all delay-300 duration-1000 ease-in-out ${
+                  className={`mt-4 grid transition-all delay-200 duration-1000 ease-in-out ${
                     currentOpen === doctor.id
                       ? 'max-h-[500px]' // a large enough value to accommodate all items (or adjust based on the number of items)
-                      : 'max-h-20' // collapse the height
-                  } grid-cols-4 gap-2 overflow-hidden`}
+                      : 'max-h-16' // collapse the height
+                  } grid-cols-10 gap-2 overflow-hidden`}
                 >
                   {doctor.times.map((time: string, idx: number) => (
                     <button
                       key={idx}
-                      className={`rounded-lg transition-all delay-300 duration-1000 ease-in-out ${currentOpen !== doctor.id && idx >= 4 ? 'opacity-50' : 'opacity-100'} border bg-white px-3 py-2 text-sm hover:bg-green-100`}
+                      className={`rounded-md ${currentOpen !== doctor.id && idx >= 10 ? 'opacity-50' : 'opacity-100'} bg-green-200/50 px-3 py-2 text-sm hover:bg-green-300`}
                       onClick={() => {
                         setBookAppointment((p) => ({
                           ...p,
