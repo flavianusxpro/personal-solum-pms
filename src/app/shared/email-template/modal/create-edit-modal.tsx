5ddcea805ce3d818e92ff1d471658f1399dd21d7
ozixprogroup <ozixprogroup@gmail.com>
2025-05-08 09:28:15 +0700

    feat: reduce loading time in email template modal from 1000ms to 500ms

--- src/app/shared/email-template/modal/create-edit-modal.tsx
+++ src/app/shared/email-template/modal/create-edit-modal.tsx
@@ -86,7 +86,7 @@ export default function CreateEditEmailTemplateModal({
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 1000);
+    }, 500);
 
     return () => clearTimeout(timer);
   }, []);
