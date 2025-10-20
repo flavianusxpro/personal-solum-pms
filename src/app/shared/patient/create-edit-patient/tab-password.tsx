2ceeb0abda9cb1962fa25aa3732fd4c5aee753f2
violaxpro <violaxprogroup@gmail.com>
2025-10-20 16:30:09 +0700

    fix : copywriting

--- src/app/shared/patient/create-edit-patient/tab-password.tsx
+++ src/app/shared/patient/create-edit-patient/tab-password.tsx
@@ -100,9 +100,9 @@ export default function TabPassword({ isView = false }: { isView?: boolean }) {
 
       <div className="mx-auto mt-10 w-full max-w-screen-2xl">
         <div className="border-b border-dashed border-muted">
-          <h2 className="rizzui-title-h2 mb-3 text-xl font-bold text-gray-900">
+          <h4 className="rizzui-title-h4 mb-3 text-base font-medium text-gray-900">
             Where you’re logged in
-          </h2>
+          </h4>
           <p className="rizzui-text-p mb-6 text-sm font-normal text-gray-500">
             We’ll alert you via olivia@untitledui.com if there is any unusual
             activity on your account.
@@ -123,9 +123,9 @@ export default function TabPassword({ isView = false }: { isView?: boolean }) {
           </svg>
           <div>
             <div className="mb-2 flex items-center gap-2">
-              <h3 className="rizzui-title-h3 text-base font-medium text-gray-900 dark:text-gray-700">
+              <h4 className="rizzui-title-h4 text-base font-medium text-gray-900 dark:text-gray-700">
                 2018 Macbook Pro 15-inch
-              </h3>
+              </h4>
               <span className="rizzui-text-span relative hidden rounded-md border border-muted py-1.5 pe-2.5 ps-5 text-xs font-semibold text-gray-900 before:absolute before:start-2.5 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full before:bg-green sm:block">
                 Active Now
               </span>
@@ -158,9 +158,9 @@ export default function TabPassword({ isView = false }: { isView?: boolean }) {
             <path d="M208,40H48A24,24,0,0,0,24,64V176a24,24,0,0,0,24,24h72v16H96a8,8,0,0,0,0,16h64a8,8,0,0,0,0-16H136V200h72a24,24,0,0,0,24-24V64A24,24,0,0,0,208,40ZM48,56H208a8,8,0,0,1,8,8v80H40V64A8,8,0,0,1,48,56ZM208,184H48a8,8,0,0,1-8-8V160H216v16A8,8,0,0,1,208,184Z"></path>
           </svg>
           <div>
-            <h3 className="rizzui-title-h3 mb-2 text-base font-medium text-gray-900 dark:text-gray-700">
+            <h4 className="rizzui-title-h4 mb-2 text-base font-medium text-gray-900 dark:text-gray-700">
               2020 Macbook Air M1
-            </h3>
+            </h4>
             <div className="flex items-center gap-2">
               <p className="rizzui-text-p text-sm font-normal text-gray-500">
                 Melbourne, Australia
